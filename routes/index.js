b6f88586f4b709e2840e059458d336b0d634929c
Alexander <alexander.lock7@gmail.com>
2017-06-20 15:03:00 +0000

    added routing and pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Alexander Lock' });
+});
+
+router.get('/home', function(req, res, next){
+  res.render('index', {title: 'Alexander Lock'})
+});
+
+router.get('/work', function(req, res, next){
+  res.render('work', {title: 'Alexander Lock'})
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', {title: 'Alexander Lock'})
+});
+
+router.get('/contact', function(req, res, next){
+  res.render('contact', {title: 'Alexander Lock'})
 });
 
 module.exports = router;
